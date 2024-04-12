9bad7537ff68497baae58a975ae4462551131aca
yangkequn <yangkequn@gmail.com>
2024-04-12 09:36:25 +0800

    refactor: rename mixin to hook

---
+++ lib/textEmbedding.go
@@ -0,0 +1,13 @@
+package lib
+
+type EmbeddingModel string
+
+const EmbeddingModelChatGpt EmbeddingModel = "chatgpt"
+
+type Text2VectorIn struct {
+	Text           []string
+	EmbeddingModel string
+}
+type Text2VectorOut struct {
+	Vector [][]float32
+}
