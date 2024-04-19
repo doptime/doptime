1ad1b87bb9b8600467816700e87bb0e1ad0bc806
yangkequn <yangkequn@gmail.com>
2024-04-19 15:55:11 +0800

    refactor: read memory size

--- dlog/getMachineName.go
+++ dlog/getMachineName.go
@@ -3,23 +3,27 @@ package dlog
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"runtime"
+	"strconv"
+	"strings"
 )
 
 func getMachineName() string {
+	var memStr string
 	host, _ := os.Hostname()
 
-	//read system memory size
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
-	totalMemMB := mem.Sys / 1024 / 1024 // convert bytes to MB
-	totalMemGB := totalMemMB / 1024     // convert MB to GB
+	if memOutput, err := exec.Command("sysctl", "-n", "hw.memsize").Output(); err == nil {
+		if totalMemBytes, err := strconv.ParseUint(strings.TrimSpace(string(memOutput)), 10, 64); err == nil {
+			totalMemMB := totalMemBytes / 1024 / 1024 // convert bytes to MB
+			totalMemGB := totalMemMB / 1024           // convert MB to GB
+			if totalMemGB >= 1 {
+				memStr = fmt.Sprintf("%dGB", totalMemGB)
+			} else {
+				memStr = fmt.Sprintf("%dMB", totalMemMB)
+			}
 
-	var memStr string
-	if totalMemGB >= 1 {
-		memStr = fmt.Sprintf("%dGB", totalMemGB)
-	} else {
-		memStr = fmt.Sprintf("%dMB", totalMemMB)
+		}
 	}
 
 	cpuCores := runtime.NumCPU()
