31510a2c344ac69769200c80b5595a08857ceaf6
yangkequn <yangkequn@gmail.com>
2024-04-21 16:44:51 +0800

    support POST GET PUT ... for all Data Commands

--- httpserve/startHttp.go
+++ httpserve/startHttp.go
@@ -4,11 +4,14 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/doptime/doptime/config"
+	"github.com/doptime/doptime/data"
 	"github.com/doptime/doptime/dlog"
 	"github.com/doptime/doptime/permission"
 	"github.com/redis/go-redis/v9"
@@ -25,7 +28,7 @@ func httpStart(path string, port int64) {
 	router.HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
 		var (
 			result     interface{}
-			b          []byte
+			bs         []byte
 			s          string
 			ok         bool
 			err        error
@@ -53,14 +56,254 @@ func httpStart(path string, port int64) {
 			err = ErrOperationNotPermited
 		} else if svcCtx.Cmd == "API" {
 			result, err = svcCtx.APiHandler()
-		} else if r.Method == "GET" {
-			result, err = svcCtx.GetHandler()
-		} else if r.Method == "POST" {
-			result, err = svcCtx.PostHandler()
-		} else if r.Method == "PUT" {
-			result, err = svcCtx.PutHandler(rds)
-		} else if r.Method == "DELETE" {
-			result, err = svcCtx.DelHandler(rds)
+		} else {
+			db := &data.Ctx[string, interface{}]{Ctx: context.Background(), Rds: rds, Key: svcCtx.Key}
+
+			switch svcCtx.Cmd {
+			// all data that appears in the form or body is json format, will be stored in paramIn["JsonPack"]
+			// this is used to support 3rd party api
+			case "GET":
+				result, err = db.Get(svcCtx.Field)
+			case "HGET":
+				result, err = db.HGet(svcCtx.Field)
+			case "HGETALL":
+				result, err = db.HGetAll()
+			case "HMGET":
+				result, err = db.HMGET(strings.Split(svcCtx.Field, ",")...)
+			case "HKEYS":
+				result, err = db.HKeys()
+			case "HEXISTS":
+				result, err = db.HExists(svcCtx.Field)
+			case "HRANDFIELD":
+				var count int
+				if count, err = strconv.Atoi(svcCtx.Req.FormValue("Count")); err != nil {
+					result, err = "", errors.New("parse count error:"+err.Error())
+				} else {
+					result, err = db.HRandField(count)
+				}
+			case "HLEN":
+				result, err = db.HLen()
+			case "HVALS":
+				result, err = db.HVals()
+			case "SISMEMBER":
+				result, err = db.SIsMember(svcCtx.Req.FormValue("Member"))
+			case "TIME":
+				result = ""
+				if tm, err := db.Time(); err == nil {
+					result = tm.UnixMilli()
+				}
+			case "ZRANGE":
+				var (
+					start, stop int64 = 0, -1
+				)
+				result = ""
+				if start, err = strconv.ParseInt(svcCtx.Req.FormValue("Start"), 10, 64); err != nil {
+				} else if stop, err = strconv.ParseInt(svcCtx.Req.FormValue("Stop"), 10, 64); err != nil {
+				} else if svcCtx.Req.FormValue("WITHSCORES") == "true" {
+					// ZRANGE key start stop [WITHSCORES==true]
+					var scores []float64
+					if result, scores, err = db.ZRangeWithScores(start, stop); err == nil {
+						result = map[string]interface{}{"members": result, "scores": scores}
+					}
+				} else {
+					// ZRANGE key start stop [WITHSCORES==false]
+					result, err = db.ZRange(start, stop)
+				}
+			case "ZRANGEBYSCORE":
+				var (
+					offset, count int64 = 0, -1
+					scores        []float64
+				)
+				result = ""
+				if Min := svcCtx.Req.FormValue("Min"); Min == "" {
+					result, err = "false", errors.New("no Min")
+				} else if Max := svcCtx.Req.FormValue("Max"); Max == "" {
+					result, err = "false", errors.New("no Max")
+				} else if offset, err = strconv.ParseInt(svcCtx.Req.FormValue("Offset"), 10, 64); err != nil {
+					result, err = "false", errors.New("parse offset error:"+err.Error())
+				} else if count, err = strconv.ParseInt(svcCtx.Req.FormValue("Count"), 10, 64); err != nil {
+					result, err = "false", errors.New("parse count error:"+err.Error())
+				} else if svcCtx.Req.FormValue("WITHSCORES") == "true" {
+					//ZRANGEBYSCORE key min max [WITHSCORES==true]
+					if result, scores, err = db.ZRangeByScoreWithScores(&redis.ZRangeBy{Min: Min, Max: Max, Offset: offset, Count: count}); err == nil {
+						result = map[string]interface{}{"members": result, "scores": scores}
+					}
+				} else {
+					//ZRANGEBYSCORE key min max [WITHSCORES==false]
+					result, err = db.ZRangeByScore(&redis.ZRangeBy{Min: Min, Max: Max, Offset: offset, Count: count})
+				}
+			case "ZREVRANGE":
+				var (
+					start, stop int64 = 0, -1
+				)
+				result = ""
+				if start, err = strconv.ParseInt(svcCtx.Req.FormValue("Start"), 10, 64); err != nil {
+				} else if stop, err = strconv.ParseInt(svcCtx.Req.FormValue("Stop"), 10, 64); err != nil {
+				} else if svcCtx.Req.FormValue("WITHSCORES") == "true" {
+					// ZREVRANGE key start stop [WITHSCORES==true]
+					cmd := db.Rds.ZRevRangeWithScores(context.Background(), db.Key, start, stop)
+					if rlts, err := cmd.Result(); err == nil {
+						var memberScoreSlice []interface{}
+						for _, rlt := range rlts {
+							memberScoreSlice = append(memberScoreSlice, rlt.Member)
+							memberScoreSlice = append(memberScoreSlice, rlt.Score)
+						}
+						result = memberScoreSlice
+					}
+				} else {
+					// ZREVRANGE key start stop [WITHSCORES==false]
+					result, err = db.ZRevRange(start, stop)
+				}
+			case "ZREVRANGEBYSCORE":
+				var (
+					offset, count int64 = 0, -1
+				)
+				if Min, Max := svcCtx.Req.FormValue("Min"), svcCtx.Req.FormValue("Max"); Min == "" || Max == "" {
+					result, err = "", errors.New("no Min or Max")
+				} else if offset, err = strconv.ParseInt(svcCtx.Req.FormValue("Offset"), 10, 64); err != nil {
+					result, err = "", errors.New("parse offset error:"+err.Error())
+				} else if count, err = strconv.ParseInt(svcCtx.Req.FormValue("Count"), 10, 64); err != nil {
+					result, err = "", errors.New("parse count error:"+err.Error())
+				} else if svcCtx.Req.FormValue("WITHSCORES") == "true" {
+					cmd := db.Rds.ZRevRangeByScoreWithScores(context.Background(), db.Key, &redis.ZRangeBy{Min: Min, Max: Max, Offset: offset, Count: count})
+					if rlts, err := cmd.Result(); err == nil {
+						var memberScoreSlice []interface{}
+						for _, rlt := range rlts {
+							memberScoreSlice = append(memberScoreSlice, rlt.Member)
+							memberScoreSlice = append(memberScoreSlice, rlt.Score)
+						}
+						result, err = memberScoreSlice, nil
+					}
+				} else {
+					//ZREVRANGEBYSCORE key max min [WITHSCORES==false]
+					result, err = db.ZRevRangeByScore(&redis.ZRangeBy{Min: Min, Max: Max, Offset: offset, Count: count})
+				}
+			case "ZCARD":
+				result, err = db.ZCard()
+			case "ZRANK":
+				result, err = db.ZRank(svcCtx.Req.FormValue("Member"))
+			case "ZCOUNT":
+				result, err = db.ZCount(svcCtx.Req.FormValue("Min"), svcCtx.Req.FormValue("Max"))
+			case "ZSCORE":
+				result, err = db.ZScore(svcCtx.Req.FormValue("Member"))
+			case "SCAN":
+				var (
+					cursor uint64
+					count  int64
+					keys   []string
+					match  string
+				)
+				result = ""
+				if cursor, err = strconv.ParseUint(svcCtx.Req.FormValue("Cursor"), 10, 64); err != nil {
+				} else if match = svcCtx.Req.FormValue("Match"); match == "" {
+				} else if count, err = strconv.ParseInt(svcCtx.Req.FormValue("Count"), 10, 64); err != nil {
+				} else if keys, cursor, err = db.Scan(cursor, match, count); err != nil {
+				} else {
+					result, err = json.Marshal(map[string]interface{}{"keys": keys, "cursor": cursor})
+				}
+			case "LRANGE":
+				var (
+					start, stop int64 = 0, -1
+				)
+				if start, err = strconv.ParseInt(svcCtx.Req.FormValue("Start"), 10, 64); err != nil {
+					result, err = "", errors.New("parse start error:"+err.Error())
+				} else if stop, err = strconv.ParseInt(svcCtx.Req.FormValue("Stop"), 10, 64); err != nil {
+					result, err = "", errors.New("parse stop error:"+err.Error())
+				} else {
+					result, err = db.LRange(start, stop)
+				}
+			case "LLEN":
+				result, err = db.LLen()
+			case "LINDEX":
+				var index int64
+				if index, err = strconv.ParseInt(svcCtx.Req.FormValue("Index"), 10, 64); err != nil {
+					result, err = "", errors.New("parse index error:"+err.Error())
+				} else {
+					result, err = db.LIndex(index)
+				}
+			case "LPOP":
+				result, err = db.LPop()
+			case "RPOP":
+				result, err = db.RPop()
+
+			case "ZADD":
+				var Score float64
+				var obj interface{}
+				if Score, err = strconv.ParseFloat(svcCtx.Req.FormValue("Score"), 64); err != nil {
+					result, err = "false", errors.New("parameter Score shoule be float")
+				} else if MsgPack := svcCtx.MsgpackBodyBytes(); len(MsgPack) == 0 {
+					result, err = "false", errors.New("missing MsgPack content")
+				} else if result, err = "true", msgpack.Unmarshal(MsgPack, &obj); err != nil {
+					result = "false"
+				} else if err = db.ZAdd(redis.Z{Score: Score, Member: obj}); err != nil {
+					result = "false"
+				}
+
+			case "SET":
+				result = "false"
+				if svcCtx.Key == "" || svcCtx.Field == "" {
+					err = ErrEmptyKeyOrField
+				} else if bytes, err := svcCtx.MsgpackBody(); err != nil {
+				} else if rds.Set(svcCtx.Ctx, svcCtx.Key+":"+svcCtx.Field, bytes, 0).Err() == nil {
+					result = "true"
+				}
+
+			case "HSET":
+				result = "false"
+				//error if empty Key or Field
+				if svcCtx.Key == "" || svcCtx.Field == "" {
+					err = ErrEmptyKeyOrField
+				} else if bs, err = svcCtx.MsgpackBody(); err != nil {
+				} else if err = rds.HSet(svcCtx.Ctx, svcCtx.Key, svcCtx.Field, bs).Err(); err == nil {
+					result = "true"
+				}
+			case "RPUSH":
+				result = "false"
+				if bs, err = svcCtx.MsgpackBody(); err != nil {
+				} else if err = rds.RPush(svcCtx.Ctx, svcCtx.Key, bs).Err(); err == nil {
+					result = "true"
+				}
+
+			case "HDEL":
+				result = "false"
+				//error if empty Key or Field
+				if svcCtx.Field == "" {
+					err = ErrEmptyKeyOrField
+				} else if err = rds.HDel(svcCtx.Ctx, svcCtx.Key, svcCtx.Field).Err(); err == nil {
+					result = "true"
+				}
+			case "DEL":
+				result = "false"
+				if err = rds.HDel(svcCtx.Ctx, svcCtx.Key, "del").Err(); err == nil {
+					result = "true"
+				}
+			case "ZREM":
+				result = "false"
+				var MemberStr = strings.Split(svcCtx.Req.FormValue("Member"), ",")
+				//convert Member to []interface{}
+				var Member = make([]interface{}, len(MemberStr))
+				for i, v := range MemberStr {
+					Member[i] = v
+				}
+
+				if len(Member) == 0 {
+					err = errors.New("no Member")
+				} else if err = rds.ZRem(svcCtx.Ctx, svcCtx.Key, Member...).Err(); err == nil {
+					result = "true"
+				}
+
+			case "ZREMRANGEBYSCORE":
+				result = "false"
+				var Min, Max = svcCtx.Req.FormValue("Min"), svcCtx.Req.FormValue("Max")
+				if Min == "" || Max == "" {
+					err = errors.New("no Min or Max")
+				} else if err = rds.ZRemRangeByScore(svcCtx.Ctx, svcCtx.Key, Min, Max).Err(); err == nil {
+					result = "true"
+				}
+			//case default
+			default:
+				result, err = nil, ErrBadCommand
+			}
 		}
 
 		if len(config.Cfg.Http.CORES) > 0 {
@@ -69,26 +312,26 @@ func httpStart(path string, port int64) {
 
 		if err == nil {
 			if svcCtx.ResponseContentType == "application/msgpack" {
-				if b, err = msgpack.Marshal(result); err != nil {
+				if bs, err = msgpack.Marshal(result); err != nil {
 					httpStatus = http.StatusInternalServerError
 				}
-			} else if b, ok = result.([]byte); ok {
+			} else if bs, ok = result.([]byte); ok {
 			} else if s, ok = result.(string); ok {
-				b = []byte(s)
+				bs = []byte(s)
 			} else {
 
-				if b, err = json.Marshal(result); err == nil {
+				if bs, err = json.Marshal(result); err == nil {
 					//json Compact b
 					var dst *bytes.Buffer = bytes.NewBuffer([]byte{})
-					if err = json.Compact(dst, b); err == nil {
-						b = dst.Bytes()
+					if err = json.Compact(dst, bs); err == nil {
+						bs = dst.Bytes()
 					}
 				}
 			}
 		}
 		//this err may be from json.marshal, so don't move it to the above else if
 		if err != nil {
-			if b = []byte(err.Error()); bytes.Contains(b, []byte("JWT")) {
+			if bs = []byte(err.Error()); bytes.Contains(bs, []byte("JWT")) {
 				httpStatus = http.StatusUnauthorized
 			} else if httpStatus == http.StatusOK {
 				// this if is needed, because  httpStatus may have already setted as StatusBadRequest
@@ -101,7 +344,7 @@ func httpStart(path string, port int64) {
 			svcCtx.Rsb.Header().Set("Content-Type", svcCtx.ResponseContentType)
 		}
 		w.WriteHeader(httpStatus)
-		w.Write(b)
+		w.Write(bs)
 	})
 
 	server := &http.Server{
