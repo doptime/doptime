323c5ea4885a59eeb6eb0b5a7c27c1a912a043a2
yangkequn <yangkequn@gmail.com>
2024-05-23 15:18:28 +0800

    fix & refactor ApiName

--- specification/serviceNames.go
+++ specification/serviceNames.go
@@ -33,19 +33,19 @@ var DisAllowedServiceNames = map[string]bool{
 // 1. the type name of the first parameter of the function
 // 2. the name give by the user
 // do not panic, because it may be called by web client. otherwise the server can be maliciously closed by the client
-func ApiName(ServiceNameOriginal string) (ServiceName string) {
+func ApiName(nmOld string) (ServiceName string) {
 	//remove postfix
-	nameLowercase := "      " + strings.ToLower(ServiceNameOriginal)
+	nameLowercase := "      " + strings.ToLower(nmOld)
 	if p := nameLowercase[len(nameLowercase)-6:]; p == "output" {
-		ServiceName = ServiceNameOriginal[:len(ServiceName)-6]
+		ServiceName = nmOld[:len(nmOld)-6]
 	} else if p := nameLowercase[len(nameLowercase)-5:]; p == "input" || p == "param" {
-		ServiceName = ServiceNameOriginal[:len(ServiceName)-5]
+		ServiceName = nmOld[:len(nmOld)-5]
 	} else if p := nameLowercase[len(nameLowercase)-4:]; p == "data" {
-		ServiceName = ServiceNameOriginal[:len(ServiceName)-4]
+		ServiceName = nmOld[:len(nmOld)-4]
 	} else if p := nameLowercase[len(nameLowercase)-3:]; p == "arg" || p == "req" || p == "src" || p == "out" {
-		ServiceName = ServiceNameOriginal[:len(ServiceName)-3]
+		ServiceName = nmOld[:len(nmOld)-3]
 	} else if p := nameLowercase[len(nameLowercase)-2:]; p == "in" {
-		ServiceName = ServiceNameOriginal[:len(ServiceName)-2]
+		ServiceName = nmOld[:len(nmOld)-2]
 	}
 
 	//remove prefix
@@ -62,7 +62,7 @@ func ApiName(ServiceNameOriginal string) (ServiceName string) {
 
 	if _, ok := DisAllowedServiceNames[ServiceName]; ok || len(ServiceName) == 0 {
 		ServiceName = big.NewInt(rand.Int63()).String()
-		dlog.Error().Msg("Service created failed when calling ApiNamed, service name " + ServiceNameOriginal + " is invalid , it's renamed to " + ServiceName)
+		dlog.Error().Msg("Service created failed when calling ApiNamed, service name " + nmOld + " is invalid , it's renamed to " + ServiceName)
 	}
 
 	//first byte of ServiceName should be lower case
