61b280d0cd3cca57024f42f871854af8f4f36fad
yangkequn <yangkequn@gmail.com>
2024-06-11 11:44:32 +0800

    remove expired code `APICodePut1`

--- dynamicdev/CodePut.go
+++ dynamicdev/CodePut.go
@@ -5,9 +5,7 @@
 package dynamicdev
 
 import (
-	"fmt"
 	"os"
-	"os/exec"
 
 	"github.com/doptime/doptime/api"
 )
@@ -39,41 +37,5 @@ var APICodePut = api.Api(func(paramIn *CodePutIn) (architectures *CodePutOut, er
 	if err != nil {
 		return nil, err
 	}
-	return architectures, nil
-}).Func
-
-var APICodePut1 = api.Api(func(paramIn *CodePutIn) (architectures *CodePutOut, err error) {
-	architectures = &CodePutOut{SourceCode: paramIn.SourceCode}
-
-	// Generate architecture information
-	if architectures.Architect, err = SourceCodeToArchitecture(paramIn.SourceCode); err != nil {
-		return nil, err
-	}
-
-	// Validate input parameters
-	if paramIn.SourceCode == "" || paramIn.FileName == "" {
-		return architectures, fmt.Errorf("source code or file name cannot be empty")
-	}
-
-	// Save the code to a local file
-	tmpFileName := paramIn.FileName + ".tmp"
-	file, err := os.Create(tmpFileName)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	if _, err = file.WriteString(paramIn.SourceCode); err != nil {
-		return nil, err
-	}
-
-	// Compare .tmp file with the original file
-	//run command code --diff file1.txt file2.txt
-	err = exec.Command("code", "--diff", paramIn.FileName, tmpFileName).Run()
-	if err != nil {
-		return nil, err
-	}
-	//focus the vscode window to allow user to make revision using vscode
-
 	return architectures, nil
 }).Func
