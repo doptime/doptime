4aeab2be94bfae5f20dbe4b55aa59d041a1ebd65
yangkequn <yangkequn@gmail.com>
2024-06-12 09:26:42 +0800

    add ApiHttpGetBytes

---
+++ libapi/http.go
@@ -0,0 +1,32 @@
+package libapi
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+// ApiHttpGetBytes 发出请求并返回响应数据
+func ApiHttpGetBytes(client *http.Client, url string, accessToken string) ([]byte, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to fetch data: %s", resp.Status)
+	}
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
