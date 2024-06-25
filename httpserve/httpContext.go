90cd7670de6e8c4de9708b1a0ff134d6b95a90f9
yangkequn <yangkequn@gmail.com>
2024-06-25 09:30:48 +0800

    simplify url specification

--- httpserve/httpContext.go
+++ httpserve/httpContext.go
@@ -43,32 +43,26 @@ func NewHttpContext(ctx context.Context, r *http.Request, w http.ResponseWriter)
 	if CmdKeyFieldsStr, err = url.QueryUnescape(CmdKeyFields[len(CmdKeyFields)-1]); err != nil {
 		return nil, err
 	}
-	if CmdKeyFields = strings.Split(CmdKeyFieldsStr, "-!"); len(CmdKeyFields) < 2 {
+	if CmdKeyFields = strings.SplitN(CmdKeyFieldsStr, "-", 2); len(CmdKeyFields) != 2 {
 		return nil, ErrIncompleteRequest
 	}
 	// cmd and key and field, i.g. /HGET/UserAvatar?F=fa4Y3oyQk2swURaJ
 	// both cmd and key are required
-	if svcContext.Cmd, svcContext.Key = CmdKeyFields[0], CmdKeyFields[1]; svcContext.Cmd == "" || svcContext.Key == "" {
+	if svcContext.Cmd, svcContext.Key = strings.ToUpper(CmdKeyFields[0]), CmdKeyFields[1]; svcContext.Cmd == "" || svcContext.Key == "" {
 		return nil, ErrIncompleteRequest
 	}
 	//url decoded already
-	svcContext.Field = r.FormValue("F")
+	svcContext.Field = r.FormValue("f")
 
 	//default response content type: application/json
-	svcContext.ResponseContentType = "application/json"
-	svcContext.RedisDataSource = "default"
-	for i, l := 2, len(CmdKeyFields); i < l; i++ {
-		if param = CmdKeyFields[i]; len(param) <= 3 {
-			continue
-		}
-		switch param[:3] {
-		case "rt~": //response content type rt=application/json
-			svcContext.ResponseContentType = param[3:]
-		case "ds~": //redis db name RDB=redisDataSource
-			svcContext.RedisDataSource = param[3:]
-		case "su~": //super user token
-			svcContext.SUToken = param[3:]
-		}
+	if svcContext.ResponseContentType, param = "application/json", r.FormValue("rt"); param != "" {
+		svcContext.ResponseContentType = param
+	}
+	if svcContext.RedisDataSource, param = "default", r.FormValue("ds"); param != "" {
+		svcContext.RedisDataSource = param
+	}
+	if svcContext.SUToken, param = "", r.FormValue("su"); param != "" {
+		svcContext.SUToken = param
 	}
 	return svcContext, nil
 }
