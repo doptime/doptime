89f84a5238625310eb098eba4e848b5531bd5c30
yangkequn <yangkequn@gmail.com>
2024-06-26 17:35:06 +0800

    refactor: convert to string for human readbility

--- libapi/DataDocs.go
+++ libapi/DataDocs.go
@@ -11,29 +11,32 @@ import (
 type DataDocsIn struct {
 }
 
-var ApiDataDocs = api.Api(func(req *DataDocsIn) (ret map[string]string, err error) {
+var ApiDataDocs = api.Api(func(req *DataDocsIn) (r string, err error) {
 	result, err := rdsdb.KeyWebDataDocs.HGetAll()
 	if err != nil {
-		return nil, err
+		return "", err
 	}
-	ret = make(map[string]string)
+	var ret strings.Builder
 	for k, v := range result {
 		keyWithFirstCharUpper := strings.ToUpper(v.KeyName[0:1]) + v.KeyName[1:]
 		keyWithFirstCharUpper = strings.Split(keyWithFirstCharUpper, ":")[0]
 		jsBytes, _ := json.Marshal(v)
+
+		ret.WriteString(k + " :\n")
 		if v.KeyType == "hash" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new hashKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new hashKey(" + string(jsBytes) + ")")
 		} else if v.KeyType == "string" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new stringKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new stringKey(" + string(jsBytes) + ")")
 		} else if v.KeyType == "list" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new listKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new listKey(" + string(jsBytes) + ")")
 		} else if v.KeyType == "set" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new setKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new setKey(" + string(jsBytes) + ")")
 		} else if v.KeyType == "zset" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new zsetKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new zsetKey(" + string(jsBytes) + ")")
 		} else if v.KeyType == "stream" {
-			ret[k] = "var key" + keyWithFirstCharUpper + " = new streamKey(" + string(jsBytes) + ")"
+			ret.WriteString("var key" + keyWithFirstCharUpper + " = new streamKey(" + string(jsBytes) + ")")
 		}
 	}
-	return ret, nil
+	// convert to toml string, do
+	return ret.String(), nil
 }).Func
