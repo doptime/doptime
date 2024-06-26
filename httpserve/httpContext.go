f2aac13bc285e79e3a0da8547f3e385c38b4870a
yangkequn <yangkequn@gmail.com>
2024-06-26 11:04:50 +0800

    fix error parse url

--- httpserve/httpContext.go
+++ httpserve/httpContext.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"net/url"
+	"path"
 	"strings"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -32,15 +33,19 @@ var ErrIncompleteRequest = errors.New("incomplete request")
 
 func NewHttpContext(ctx context.Context, r *http.Request, w http.ResponseWriter) (httpCtx *HttpContext, err error) {
 	var (
-		CmdKeyFields           []string
-		param, CmdKeyFieldsStr string
+		CmdKeyFields                                  []string
+		param, CmdKeyFieldsStr, pathStr, pathLastPart string
 	)
 	svcContext := &HttpContext{Req: r, Rsb: w, Ctx: ctx}
-	//i.g. https://url.com/rSvc/HGET=UserAvatar=fa4Y3oyQk2swURaJ?Queries=*&RspType=image/jpeg
-	if CmdKeyFields = strings.Split(r.URL.RawPath, "/"); len(CmdKeyFields) < 1 {
+
+	//i.g. https://url.com/rSvc/HGET-UserAvatar=fa4Y3oyQk2swURaJ?Queries=*&RspType=image/jpeg
+	if pathStr = r.URL.Path; r.URL.RawPath != "" {
+		pathStr = r.URL.RawPath
+	}
+	if pathLastPart = path.Base(pathStr); pathLastPart == "" {
 		return nil, ErrIncompleteRequest
 	}
-	if CmdKeyFieldsStr, err = url.QueryUnescape(CmdKeyFields[len(CmdKeyFields)-1]); err != nil {
+	if CmdKeyFieldsStr, err = url.QueryUnescape(pathLastPart); err != nil {
 		return nil, err
 	}
 	if CmdKeyFields = strings.SplitN(CmdKeyFieldsStr, "-", 2); len(CmdKeyFields) != 2 {
