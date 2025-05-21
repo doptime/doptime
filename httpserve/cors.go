5fc7d181c6788e07910bec5b79ac444d9f18db6a
yangkequn <yangkequn@gmail.com>
2025-05-21 15:39:29 +0800

    fix CorsChecked

--- httpserve/cors.go
+++ httpserve/cors.go
@@ -3,19 +3,30 @@ package httpserve
 import (
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/doptime/config/cfghttp"
 )
 
 func CorsChecked(r *http.Request, w http.ResponseWriter) bool {
-	if r.Method == "OPTIONS" && len(cfghttp.CORES) > 0 {
+	if r.Method == "OPTIONS" {
+		// Allow all methods and headers for OPTIONS requests
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization")
-		w.Header().Set("Access-Control-Allow-Origin", cfghttp.CORES)
-		w.Header().Set("Access-Control-Max-Age", strconv.Itoa(30*86400))
-		w.Header().Set("Content-Type", "text/html; charset=ascii")
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("CORS OK"))
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, Accept-Language, X-CSRF-Token, Authorization, Rt, Origin, Refer, User-Agent")
+
+		// Handle multiple allowed origins or wildcard
+		origin := r.Header.Get("Origin")
+		if origin != "" {
+			if cfghttp.CORES == "*" {
+				// Allow all origins
+				w.Header().Set("Access-Control-Allow-Origin", "*")
+			} else if strings.Contains(cfghttp.CORES, origin) {
+				w.Header().Set("Access-Control-Allow-Origin", origin)
+			}
+		}
+
+		w.Header().Set("Access-Control-Max-Age", strconv.Itoa(30*86400)) // 30 days
+		w.WriteHeader(http.StatusNoContent)
 		return true
 	}
 	return false
