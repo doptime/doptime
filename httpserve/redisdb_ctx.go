83fac29399c8be58be2ba6c9b2c292d17ba75229
yangkequn <yangkequn@gmail.com>
2026-01-10 19:34:12 +0800

    proper calling mapper.Decode

--- httpserve/redisdb_ctx.go
+++ httpserve/redisdb_ctx.go
@@ -1,25 +1,57 @@
 package httpserve
 
 import (
+	"reflect"
+
 	"github.com/doptime/doptime/utils/mapper"
 	"github.com/doptime/redisdb"
 )
 
 func (req *DoptimeReqCtx) ToValue(key redisdb.IHttpKey, msgpack []byte) (value interface{}, err error) {
 	value, err = key.DeserializeValue(msgpack)
-	mapper.Decode(req.Params, &value)
+	if err != nil {
+		return nil, err
+	}
+
+	// Avoid double reference if value is already a pointer
+	if isPtr(value) {
+		err = mapper.Decode(req.Params, value)
+	} else {
+		err = mapper.Decode(req.Params, &value)
+	}
+
 	return value, err
 }
+
 func (req *DoptimeReqCtx) ToValues(key redisdb.IHttpKey, msgpack []string) (value []interface{}, err error) {
 	value, err = key.DeserializeValues(msgpack)
-	for i, v := range value {
-		//这个地方有错误，fields 的值会不断变化
-		req.Params["@field"] = req.Fields[i]
-		err = mapper.Decode(req.Params, &v)
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range value {
+		if i < len(req.Fields) {
+			req.Params["@field"] = req.Fields[i]
+		}
+
+		// Access via index to modify the actual element, not the loop copy
+		if isPtr(value[i]) {
+			err = mapper.Decode(req.Params, value[i])
+		} else {
+			err = mapper.Decode(req.Params, &value[i])
+		}
+
 		if err != nil {
 			return value, err
 		}
 	}
 
 	return value, err
+}
+
+func isPtr(v interface{}) bool {
+	if v == nil {
+		return false
+	}
+	return reflect.ValueOf(v).Kind() == reflect.Ptr
 }
